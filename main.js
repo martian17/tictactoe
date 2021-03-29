07ac10a62dbd17e4cc1affd1970e04b0d1043618
Yutaro Yoshii <yakumodanshi@gmail.com>
2021-03-30 00:45:48 +0900

    added js servers

--- main.js
+++ main.js
@@ -1,14 +1,7 @@
-
-
-var canvas = document.createElement("canvas");
-document.body.appendChild(canvas);
-canvas.width = 500;
-canvas.height = 500;
-var ctx = canvas.getContext("2d");
-
-
-var TicTacToe = function(){
-    var fieldWidth = 3;
+var TicTacToe = function(canvas,fieldWidth){
+    var width = canvas.width;
+    var height = canvas.height;
+    var ctx = canvas.getContext("2d");
     //rendering
 
     /*
@@ -22,8 +15,8 @@ var TicTacToe = function(){
     render(data){
       for (i=0; i < data.length; i++){
         for (j=0; j < data[0].length; j++){
-          ctx.strokeRect();
-          
+          ctx.strokeRect(20+i*50, 20+j*50, 50, 50);
+
         }
       }
     }
@@ -33,40 +26,33 @@ var TicTacToe = function(){
     var placeable = false;
 
     canvas.addEventListener("click",function(e){
-      var x = e.clientX-window.scrollX+this.offsetLeft;
-      var y = e.clientY-window.scrollY+this.offsetTop;
-      var xid = Math.floor(x/fieldWidth);
-      var yid = Math.floor(y/fieldWidth);
-      chooseAction(xid,yid);
+        var x = e.clientX-window.scrollX+this.offsetLeft;
+        var y = e.clientY-window.scrollY+this.offsetTop;
+        var xid = Math.floor(x/fieldWidth);
+        var yid = Math.floor(y/fieldWidth);
+        chooseAction(xid,yid);
     });
 
     var chooseAction = function(xid,yid){
-      if(placeable)place(xid,yid,currentUser);
+        if(placeable)place(xid,yid,currentUser);
     };
 
     //mechanics
 
     var data = [];
 
-    var place = function(xid,yid,user){
-      
+    var initiateData = function(){
+
     }
-}
+
+    var place = function(xid,yid,user){
+
+    }
+};
 
 
-/*
-
-<div id="parent">
-    <div id="cell">O</div>
-    <div id="cell">X</div>
-    <div id="cell">-</div>
-    <div id="cell"></div>
-</div>
-
-document.getElementById("parent").children[5];
-*/
-
-
-
-var a = "asdfasd";
-
+var canvas = document.createElement("canvas");
+document.body.appendChild(canvas);
+canvas.width = 500;
+canvas.height = 500;
+var ttt = new TicTacToe(canvas,3);
